a59399bf7837265c0dd8ac7ec8aaccdb583b1d08
Manjeet Singh <manjeetazim@gmail.com>
2022-11-02 15:02:17 +0530

    My Data science codes in python

---
+++ .idea/nokia.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+data = pd.read_csv(r'C:\Users\manje\Downloads\Nokia_Dataset.csv')
+#print(data)
+print(data.describe())
